fa191d83497868c70f419149a5fdc59229ee3535
tomer <tomerraviv95@gmail.com>
2022-08-21 14:17:37 +0300

    Finished with dropout, starting to work on the ensemble

--- python_code/detectors/vnet/vnet_detector.py
+++ python_code/detectors/vnet/vnet_detector.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 
 from python_code import DEVICE
+from python_code.utils.constants import Phase
 
 HIDDEN1_SIZE = 75
 HIDDEN2_SIZE = 16
@@ -48,11 +49,12 @@ class VNETDetector(nn.Module):
         self._initialize_dnn(dropout_rate)
 
     def _initialize_dnn(self, dropout_rate):
-        layers = [nn.Linear(1, HIDDEN1_SIZE),
-                  nn.Dropout(dropout_rate),
-                  nn.ReLU(),
-                  nn.Linear(HIDDEN1_SIZE, self.n_states)]
-        self.net = nn.Sequential(*layers).to(DEVICE)
+        linear_layer1 = nn.Linear(1, HIDDEN1_SIZE)
+        dropout = nn.Dropout(dropout_rate)
+        relu = nn.ReLU()
+        linear_layer2 = nn.Linear(HIDDEN1_SIZE, self.n_states)
+        self.train_layers = nn.ModuleList([linear_layer1, relu, linear_layer2]).to(DEVICE)
+        self.test_layers = nn.ModuleList([linear_layer1, dropout, relu, linear_layer2]).to(DEVICE)
 
     def forward(self, rx: torch.Tensor, phase: str) -> torch.Tensor:
         """
@@ -64,18 +66,22 @@ class VNETDetector(nn.Module):
         """
         # initialize input probabilities
         in_prob = torch.zeros([1, self.n_states]).to(DEVICE)
-        priors = self.net(rx)
 
-        if phase == 'val':
+        if phase == Phase.TEST:
+            out = rx.clone()
+            for layer in self.test_layers:
+                out = layer(out)
             detected_word = torch.zeros(rx.shape).to(DEVICE)
             for i in range(rx.shape[0]):
                 # get the lsb of the state
                 detected_word[i] = torch.argmin(in_prob, dim=1) % 2
                 # run one Viterbi stage
-                out_prob = acs_block(in_prob, -priors[i], self.transition_table, self.n_states)
+                out_prob = acs_block(in_prob, -out[i], self.transition_table, self.n_states)
                 # update in-probabilities for next layer
                 in_prob = out_prob
-
             return detected_word
         else:
-            return priors
+            out = rx.clone()
+            for layer in self.train_layers:
+                out = layer(out)
+            return out
