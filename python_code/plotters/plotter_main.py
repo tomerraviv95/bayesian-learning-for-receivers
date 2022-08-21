3203bca7fbecf1243a9f4ec48de32899c6e2ca50
tomer <tomerraviv95@gmail.com>
2022-08-21 15:23:44 +0300

    Adapted the plotter to support the ensemble method

--- python_code/plotters/plotter_main.py
+++ python_code/plotters/plotter_main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     trial_num = 1  # number of trials per point estimate, used to reduce noise by averaging results of multiple runs
     run_params_obj = RunParams(run_over=run_over,
                                trial_num=trial_num)
-    label_name = PlotType.Model_VS_Ensemble_By_SNR # Always_VS_Random_By_SNR, Always_VS_Random_By_Block
+    label_name = PlotType.Model_VS_Ensemble_By_SNR
 
     print(label_name.name)
     params_dicts, methods_list, values, xlabel, ylabel, plot_type = get_config(label_name.name)
