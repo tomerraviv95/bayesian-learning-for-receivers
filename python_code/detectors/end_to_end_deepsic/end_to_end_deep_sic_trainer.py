16c1d70532c5dfc3f7b69144c2e78993542d1873
tomer <tomerraviv95@gmail.com>
2023-01-28 11:23:35 +0200

    Cleaned up basi deep sic code

--- python_code/detectors/end_to_end_deepsic/end_to_end_deep_sic_trainer.py
+++ python_code/detectors/end_to_end_deepsic/end_to_end_deep_sic_trainer.py
@@ -4,30 +4,16 @@ import torch
 from torch import nn
 
 from python_code import DEVICE
-from python_code.channel.channels_hyperparams import N_ANT, N_USER, MODULATION_NUM_MAPPING
-from python_code.channel.modulator import BPSKModulator, QPSKModulator
-from python_code.detectors.end_to_end_deepsic.end_to_end_deep_sic_detector import E2EDeepSICDetector
-from python_code.detectors.trainer import Trainer
+from python_code.detectors.deepsic_detector import DeepSICDetector
+from python_code.detectors.deepsic_trainer import DeepSICTrainer
 from python_code.utils.config_singleton import Config
-from python_code.utils.constants import HALF, ModulationType
-from python_code.utils.trellis_utils import prob_to_QPSK_symbol
 
 conf = Config()
 ITERATIONS = 2
 EPOCHS = 400
 
 
-def prob_to_BPSK_symbol(p: torch.Tensor) -> torch.Tensor:
-    """
-    prob_to_symbol(x:PyTorch/Numpy Tensor/Array)
-    Converts Probabilities to BPSK Symbols by hard threshold: [0,0.5] -> '-1', [0.5,1] -> '+1'
-    :param p: probabilities vector
-    :return: symbols vector
-    """
-    return torch.sign(p - HALF)
-
-
-class EndToEndDeepSICTrainer(Trainer):
+class EndToEndDeepSICTrainer(DeepSICTrainer):
     """Form the trainer class.
 
     Keyword arguments:
@@ -35,34 +21,17 @@ class EndToEndDeepSICTrainer(Trainer):
     """
 
     def __init__(self):
-        self.memory_length = 1
-        self.n_user = N_USER
-        self.n_ant = N_ANT
-        self.lr = 5e-3
+
         super().__init__()
 
     def __str__(self):
         return 'End-To-End DeepSIC'
 
     def _initialize_detector(self):
-        detectors_list = [[E2EDeepSICDetector().to(DEVICE) for _ in range(ITERATIONS)] for _ in
+        detectors_list = [[DeepSICDetector().to(DEVICE) for _ in range(ITERATIONS)] for _ in
                           range(self.n_user)]  # 2D list for Storing the DeepSIC Networks
         flat_detectors_list = [detector for sublist in detectors_list for detector in sublist]
         self.detector = nn.ModuleList(flat_detectors_list)
-
-    def calc_loss(self, est: torch.Tensor, tx: torch.IntTensor) -> torch.Tensor:
-        """
-        Cross Entropy loss - distribution over states versus the gt state label
-        """
-        return self.criterion(input=est, target=tx.long())
-
-    @staticmethod
-    def preprocess(rx: torch.Tensor) -> torch.Tensor:
-        if conf.modulation_type == ModulationType.BPSK.name:
-            return rx.float()
-        elif conf.modulation_type == ModulationType.QPSK.name:
-            y_input = torch.view_as_real(rx[:, :N_ANT]).float().reshape(rx.shape[0], -1)
-            return torch.cat([y_input, rx[:, N_ANT:].float()], dim=1)
 
     def train_model(self, single_model: nn.Module, tx: torch.Tensor, rx: torch.Tensor):
         """
@@ -78,16 +47,6 @@ class EndToEndDeepSICTrainer(Trainer):
         for user in range(self.n_user):
             cur_loss += self.train_model(self.detector[user * ITERATIONS + ITERATIONS - 1], tx_all[user], rx_all[user])
         return cur_loss
-
-    def _initialize_probs_for_training(self, tx):
-        if conf.modulation_type == ModulationType.BPSK.name:
-            probs_vec = HALF * torch.ones(tx.shape).to(DEVICE)
-        elif conf.modulation_type == ModulationType.QPSK.name:
-            probs_vec = 1/4 * torch.ones(tx.shape).to(DEVICE).unsqueeze(-1).repeat(
-                [1, 1, MODULATION_NUM_MAPPING[conf.modulation_type] - 1])
-        else:
-            raise ValueError("No such constellation!")
-        return probs_vec
 
     def _online_training(self, tx: torch.Tensor, rx: torch.Tensor):
         """
@@ -112,53 +71,6 @@ class EndToEndDeepSICTrainer(Trainer):
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-
-    def forward(self, rx: torch.Tensor, h: torch.Tensor = None) -> torch.Tensor:
-        # detect and decode
-        probs_vec = self._initialize_probs_for_infer()
-        for i in range(ITERATIONS):
-            probs_vec = self.calculate_posteriors(self.detector, i + 1, probs_vec, rx)
-        confidence_word, confident_bits, detected_word = self.compute_output(probs_vec)
-        return detected_word, (confident_bits, confidence_word)
-
-    def _initialize_probs_for_infer(self):
-        if conf.modulation_type == ModulationType.BPSK.name:
-            probs_vec = HALF * torch.ones(conf.block_length - conf.pilot_size, N_ANT).to(DEVICE).float()
-        elif conf.modulation_type == ModulationType.QPSK.name:
-            probs_vec = 1/4 * torch.ones((conf.block_length - 2 * conf.pilot_size) // 2, N_ANT).to(
-                DEVICE).unsqueeze(-1).repeat([1, 1, MODULATION_NUM_MAPPING[conf.modulation_type] - 1]).float()
-        else:
-            raise ValueError("No such constellation!")
-        return probs_vec
-
-    def compute_output(self, probs_vec):
-        if conf.modulation_type == ModulationType.BPSK.name:
-            detected_word = BPSKModulator.demodulate(prob_to_BPSK_symbol(probs_vec.float()))
-            new_probs_vec = torch.cat([probs_vec.unsqueeze(dim=2), (1 - probs_vec).unsqueeze(dim=2)], dim=2)
-            confident_bits = 1 - torch.argmax(new_probs_vec, dim=2)
-            confidence_word = torch.amax(new_probs_vec, dim=2)
-        elif conf.modulation_type == ModulationType.QPSK.name:
-            detected_word = QPSKModulator.demodulate(prob_to_QPSK_symbol(probs_vec.float()))
-            new_probs_vec = torch.cat([probs_vec, (1 - probs_vec.sum(dim=2)).unsqueeze(dim=2)], dim=2)
-            confident_bits = detected_word
-            confidence_word = torch.amax(new_probs_vec, dim=2)
-        else:
-            raise ValueError("No such constellation!")
-        return confidence_word, confident_bits, detected_word
-
-    def prepare_data_for_training(self, tx: torch.Tensor, rx: torch.Tensor, probs_vec: torch.Tensor) -> [
-        torch.Tensor, torch.Tensor]:
-        """
-        Generates the data for each user
-        """
-        tx_all = []
-        rx_all = []
-        for k in range(self.n_user):
-            idx = [user_i for user_i in range(self.n_user) if user_i != k]
-            current_y_train = torch.cat((rx, probs_vec[:, idx].reshape(rx.shape[0], -1)), dim=1)
-            tx_all.append(tx[:, k])
-            rx_all.append(current_y_train)
-        return tx_all, rx_all
 
     def calculate_posteriors(self, model: nn.ModuleList, i: int, probs_vec: torch.Tensor,
                              rx: torch.Tensor) -> torch.Tensor:
