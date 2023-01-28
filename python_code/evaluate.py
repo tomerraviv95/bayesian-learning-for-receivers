54d4f52fc53b87249df6860dfe61ebddd3672106
tomer <tomerraviv95@gmail.com>
2023-01-28 11:26:09 +0200

    Cleaned up basi deep sic code

--- python_code/evaluate.py
+++ python_code/evaluate.py
@@ -3,11 +3,11 @@ import os
 from python_code.detectors.bayesian_deepsic.bayesian_deep_sic_trainer import \
     BayesianDeepSICTrainer
 from python_code.detectors.bayesian_dnn.bayesian_dnn_trainer import BayesianDNNTrainer
-from python_code.detectors.dnn.dnn_trainer import DNNTrainer
 from python_code.detectors.deepsic.end_to_end_deepsic.end_to_end_deep_sic_trainer import EndToEndDeepSICTrainer
+from python_code.detectors.deepsic.seq_deepsic.seq_deep_sic_trainer import SeqDeepSICTrainer
+from python_code.detectors.dnn.dnn_trainer import DNNTrainer
 from python_code.detectors.model_based_bayesian_deepsic.model_based_bayesian_deep_sic_trainer import \
     ModelBasedBayesianDeepSICTrainer
-from python_code.detectors.deepsic.seq_deepsic import SeqDeepSICTrainer
 from python_code.utils.config_singleton import Config
 from python_code.utils.constants import DetectorType
 
